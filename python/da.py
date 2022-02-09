ee61d8c071fefbdee78fcc21f642d47887d7c81b
Alex Klymov <junkiewind@gmail.com>
2022-02-09 16:25:52 +0200

    python script rrerfactored to provide counts

---
+++ python/da.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+skillsData = pd.read_csv("../db/skills.csv")
+# print(skillsData)
+allSkills = skillsData['skill']
+uniqueSkills = pd.unique(skillsData['skill'])
+uniqueUsers = pd.unique(skillsData['user'])
+# print("Знайдено ", len(uniqueSkills), " унікальних навичок: ", uniqueSkills)
+
+skills_count = skillsData.groupby('skill')['skillId'].count()
+combined = skills_count.to_json(force_ascii=False, orient='table')[:-1]
+combined += ',{"totalSkills" : ' + str(len(uniqueSkills)) + '},'
+combined += '{"totalUsers" : ' + str(len(uniqueUsers)) + '}'
+print(combined)
+# print(skills_count.to_json(force_ascii=False, orient='table'))
+
+# #viz
+
+# skills_count.plot(kind='pie')
+# plt.show()
