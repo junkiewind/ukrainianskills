90a92dc2a865eca1b88fa06ea6d188092239f646
Alex Klymov <junkiewind@gmail.com>
2022-02-14 12:06:40 +0200

    test https  added pointing to certbot certs on DO

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express')
+const https = require(`https`);
+const http = require(`http`);
+const fs = require(`fs`);
+const os = require("os");
+const hostname = os.hostname();
+console.log('hostname', hostname)
+
+
 
 
 const app = express()
@@ -6,6 +14,30 @@ const app = express()
 const {
     spawn
 } = require('child_process');
+
+if (hostname === "Alexanders-MacBook-Pro.local") {
+http
+    .createServer(app)
+    .listen(80, () => {
+        console.log('server is runing at port 80')
+    });
+}
+
+else {
+    const options = {
+    key: fs.readFileSync(`/etc/letsencrypt/live/klymov.design/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/klymov.design/fullchain.pem`)
+};
+https
+    
+    .createServer(app, options)
+    .listen(80, () => {
+        console.log('server is runing at port 80')
+    });
+}
+
+
+
 app.set("view enjine", "ejs")
 // app.use(express.urlencoded({
 //     extended: true
@@ -22,12 +54,12 @@ app.get('/', (req, res) => {
 
 app.get('/data', (req, res) => {
     const python = spawn('python3', ['python/datanalysis.py']);
-    console.log(python)
+    // console.log(python)
     // collect data from script
     // res.type('json');
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
-        
+
         dataToSend = data.toString();
         //   dataToSend = JSON.parse(dataToSend);
 
@@ -36,7 +68,7 @@ app.get('/data', (req, res) => {
     python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
         // send data to browser
-        console.log('dataToSend', dataToSend)
+        // console.log('dataToSend', dataToSend)
         res.send(dataToSend);
     });
 
@@ -44,8 +76,8 @@ app.get('/data', (req, res) => {
 
 app.post('/skills', (req, res) => {
     const skills = req.body;
-    
-    console.log(`${skills.skillsInput}, ${skills.username}, ${skills.usermail}, ${skills.usercity}`)
+
+    // console.log(`${skills.skillsInput}, ${skills.username}, ${skills.usermail}, ${skills.usercity}`)
     const python = spawn('python3', ['python/userinput.py', skills.skillsInput, skills.username, skills.usermail, skills.usercity])
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
@@ -59,7 +91,7 @@ app.post('/skills', (req, res) => {
 
 app.use(express.static("public"))
 
-const port = 80
-app.listen(port, () => {
-    console.log(`Experss in running on port  ${port}`)
-})
+// const port = 80
+// app.listen(port, () => {
+//     console.log(`Experss in running on port  ${port}`)
+// })
